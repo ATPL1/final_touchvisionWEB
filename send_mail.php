ca9e097342a57c1da6a94e5ca09b4ec7ee713c3a
ATPL1 <43164864+ATPL1@users.noreply.github.com>
2018-09-19 17:18:49 +0530

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,75 @@
+<?php
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "anuvad.agrawalte@gmail.com";
+
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+
+$error_page = "error.html";
+$thankyou_page = "thank_you.html";
+
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$email_address = $_REQUEST['email_address'];
+$subject = $_REQUEST['subject'] ;
+$message = $_REQUEST['message'] ;
+$firstname = $_REQUEST['firstname'] ;
+$msg = 
+"First Name: " . $firstname . "\r\n" . 
+"Email: " . $email_address . "\r\n" . 
+"Subject: " . $subject . "\r\n" .
+"Message: " . $message; 
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+
+// If the form fields are empty, redirect to the error page.
+if (empty($firstname) || empty($email_address)) {
+header( "Location: $error_page" );
+}
+
+/* 
+If email injection is detected, redirect to the error page.
+If you add a form field, you should add it here.
+*/
+elseif ( isInjected($email_address) || isInjected($firstname)  || isInjected($subject) || isInjected($message)) {
+header( "Location: $error_page" );
+}
+
+// If we passed all previous tests, send the email then redirect to the thank you page.
+else {
+
+	mail( $webmaster_email, "ContactUs Form Results", $msg );
+
+	header( "Location: $thankyou_page" );
+}
+?>
